b1477930c419296f459737e082d9bb86b282e1e9
tianrui.zhang <tianrui.zhang@ly.com>
2020-09-10 17:13:11 +0800

    提交代码

--- routes/todoList.js
+++ routes/todoList.js
@@ -4,13 +4,17 @@ import todoListControl from '../controller/todoList'
 
 router.prefix('/todoList')
 
+// 获取所有的任务
 router.get('/getAllTodoList', async (ctx, next) => {
   ctx.response.body = await todoList.find()
   next()
 })
 
+// 增加
 router.post('/addTodoList', todoListControl.addTodoList)
+// 删除
 router.post('/deleteTodoList', todoListControl.deleteTodoListItem)
+// 修改
 router.post('/updateTodoListItem', todoListControl.updateTodoListItem)
 
 
--- routes/user.js
+++ routes/user.js
@@ -4,13 +4,20 @@ import userControl from '../controller/user'
 
 router.prefix('/users')
 
+//  获取所有用户
 router.get('/getAllUsers', async (ctx, next) => {
   ctx.response.body = await Users.find()
   next()
 })
 
+
+// 注册
 router.post('/register', userControl.register)
+
+// 登录
 router.post('/login', userControl.login)
+
+// 获取用用户信息
 router.post('/getUserInfo', userControl.userInfo)
 
 export default router
